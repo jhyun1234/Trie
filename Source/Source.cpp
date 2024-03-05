b2901eb296e197e8659f598d9fdde658436c6c06
jhyun1234 <anjyounl24@gmail.com>
2024-03-05 19:45:53 +0900

    2024.3.5 Trie 자료구조

---
+++ Source/Source.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    Node* alphabet[26] = { NULL, };
+    bool finish = false;
+};
+
+class Trie
+{
+public:
+    Node* root;
+    Trie()
+    {
+        root = new Node();
+    }
+    void Insert(const char* content)
+    {
+        Node* currentNode = root;
+        for (int i = 0; i < strlen(content); i++)
+        {
+            if (currentNode->alphabet[content[i] - 'a']==NULL)
+            {
+                currentNode->alphabet[content[i] - 'a'] = new Node;
+                currentNode = currentNode->alphabet[content[i] - 'a'];
+            }
+            else
+            {
+                currentNode= currentNode->alphabet[content[i] - 'a'];
+            }
+        }
+
+        currentNode->finish = true;
+    }
+};
+
+
+
+int main()
+{
+    
+}
+
