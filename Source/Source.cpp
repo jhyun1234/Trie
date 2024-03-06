a3378dd9eaf0dcbae3b507601c845a74be23bb5b
asdf <anjyounl24@gmail.com>
2024-03-06 19:46:19 +0900

    2024.3.6 Delete Trie

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -34,13 +34,75 @@ public:
         }
 
         currentNode->finish = true;
+            
     }
+
+    bool Find(const char* content)
+    {
+        Node* currentNode = root;
+
+        for (int i = 0; i < strlen(content); i++)
+        {
+            if (currentNode->alphabet[content[i] - 'a'] == NULL)
+            {
+                return false;
+            }
+            else
+            {
+                currentNode = currentNode->alphabet[content[i] - 'a'];
+            }
+ 
+        }
+        if (currentNode->finish == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void Delete(const char* content)
+    {
+        Node* currentNode = root;
+        Node* nextNode = nullptr;
+
+        for (int i = 0; i < strlen(content); i++)
+        {
+            if (currentNode->alphabet[content[i] - 'a'] == NULL)
+            {
+                return;
+            }
+
+            nextNode = currentNode->alphabet[content[i] - 'a']->alphabet[content[i + 1] - 'a'];
+
+            delete currentNode->alphabet[content[i]-'a'];
+
+            if (i == 0)
+            { 
+                root->alphabet[content[i] - 'a'] = nullptr;
+            }
+
+            currentNode = nextNode;
+        }
+    }
+
 };
 
 
 
 int main()
 {
-    
+    Trie trie;
+
+    trie.Insert("tree");
+
+    cout << trie.Find("tree") << endl;
+
+    trie.Delete("tree");
+
+    cout << trie.Find("tree") << endl;
 }
 
